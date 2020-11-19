d3ba6bbb5c91acc2ed22c0e607ebde51cbe517e5
manisha3ch <73401128+manisha3ch@users.noreply.github.com>
2020-11-19 11:28:02 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,25 @@
+// Declare the variables
+
+// Preload function to load Images, Animation, Sound Files
+function preload(){
+ 
+}
+
+// Setup function 
+function setup(){
+
+}
+
+// Draw function
+function draw(){
+
+}
+
+// User Defined functions to display / give behaviors to enemy and fruits
+function Enemy(){
+    
+}
+
+function fruits(){
+ 
+}
